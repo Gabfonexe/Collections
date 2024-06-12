22db897b979631c7a9d1e2a5cf27e9d187b00608
Gabfonexe <bielfrancisco23@gmail.com~>
2024-06-12 16:55:54 -0300

    adicionando

--- src/StreamAPI/Desafios/ex01.java
+++ src/StreamAPI/Desafios/ex01.java
@@ -2,15 +2,19 @@ package StreamAPI.Desafios;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 public class ex01 {
   
-  List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+  public static void main(String[] args) {
+    List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-  //Predicate<Integer> maisDeCincoCaracteres = numero -> numero.compareTo(numeros) ;
-
-   Function<Integer, Integer> ordenar = numero -> numero * 2;
-
+    
+    numeros.forEach(n -> {
+      if (n > 0 ){
+        numeros.sort(null);
+        System.out.println(n);
+      }
+    }
+   );
+  }
 }
